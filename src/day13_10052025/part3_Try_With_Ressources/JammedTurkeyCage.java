3c2614bad139d5590b6836dcef05165536c4782b
Mohamed Amine MEZGHICH <git@mezghich.com>
2025-05-10 12:29:08 +0100

    fin chapitre 6

--- src/day13_10052025/part3_Try_With_Ressources/JammedTurkeyCage.java
+++ src/day13_10052025/part3_Try_With_Ressources/JammedTurkeyCage.java
@@ -6,10 +6,19 @@ public class JammedTurkeyCage implements AutoCloseable {
     }
 
     public static void main(String[] args) {
-        try (JammedTurkeyCage t = new JammedTurkeyCage()) {
+        /*
+    	try (JammedTurkeyCage t = new JammedTurkeyCage()) {
             System.out.println("put turkeys in");
         } catch (IllegalStateException e) {
             System.out.println("caught: " + e.getMessage());
+        }*/
+    	
+    	try (JammedTurkeyCage t = new JammedTurkeyCage()) {
+            throw new IllegalStateException("turkeys ran off");
+        } catch (IllegalStateException e) {
+            System.out.println("caught: " + e.getMessage());
+            for (Throwable t : e.getSuppressed())
+                System.out.println(t.getMessage());
         }
     }
 }
